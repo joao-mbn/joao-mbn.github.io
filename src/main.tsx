7f0c4d68b0fdf65a68e42b8d0664a12f03749271
João <jbatista@buckman.com>
2022-12-18 12:42:54 -0300

    removed unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/chart.css';
 import './styles/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
