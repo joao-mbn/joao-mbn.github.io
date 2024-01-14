dfa69e9b99d04572096fa40532259fd95a1d69d7
João Nascimento <joaombn97@gmail.com>
2024-01-13 23:13:49 -0300

    Creating the project from the ground up with next JS.

--- src/enums/Device.ts
+++
@@ -1,5 +0,0 @@
-export const enum DeviceEnum {
-  MOBILE,
-  TABLET,
-  DESKTOP,
-}
--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const MOBILE_SIZE = 360;
-export const TABLET_SIZE = 640;
-export const DESKTOP_SIZE = 1024;
-
-export const PIXEL_TO_REM = 10;
