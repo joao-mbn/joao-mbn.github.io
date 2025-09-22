af399fb5b0145f89750bb3b42060205bf7ddebe5
João Nascimento <joaombn97@gmail.com>
2025-09-22 10:02:27 -0300

    update home to consume data from back-end.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { ExternalLink, HexagonImageContainer, Page, Paragraph } from './components';
 import { MyAge } from './components/MyAge';
+import { getHomeContent } from './content';
 import { EMAIL } from './utils/constants';
 
-export default function Home() {
+export default async function Home() {
+  const content = await getHomeContent();
+
   return (
     <Page>
       <section className="mx-auto flex flex-col-reverse items-center gap-2 sm:flex-row sm:items-start">
@@ -14,13 +17,12 @@ export default function Home() {
           />
         </HexagonImageContainer>
         <section className="flex flex-col">
-          <h1 className="text-biggest">Hello there!</h1>
+          <h1 className="text-biggest">{content.greeting}</h1>
           <Paragraph>
-            I&apos;m João, a natural problem solver and Devil&apos;s Advocate with <MyAge /> years of experience.
+            {content.introduction} <MyAge /> {content.introductionSuffix}
           </Paragraph>
           <Paragraph>
-            My most relevant skill is to learn fast. Come to me with some idea, proposal, worldview or argument and we
-            will be set for a good talk. <ExternalLink href={`mailto:${EMAIL}`}>Send me a message</ExternalLink>.
+            {content.description} <ExternalLink href={`mailto:${EMAIL}`}>{content.cta}</ExternalLink>.
           </Paragraph>
         </section>
       </section>
