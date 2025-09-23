728f4509ec151dc6ab93c73e40d82d31d774229c
João Nascimento <joaombn97@gmail.com>
2025-09-23 13:40:15 -0300

    removed unused constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,9 +1,7 @@
 export const EMAIL = 'joaombn97@gmail.com';
-export const PHONE = '+55 (21) 99350-4046';
 export const GITHUB = 'https://github.com/joao-mbn';
 export const LINKEDIN = 'https://linkedin.com/in/joaomarcosbatistanascimento';
-export const RADIX = 'https://www.radixeng.com/';
-export const THESIS = 'https://pantheon.ufrj.br/handle/11422/18995?locale=en';
+export const THESIS = 'http://hdl.handle.net/11422/18995';
 export const ETR_OPTIMIZATION_GITHUB = 'https://github.com/joao-mbn/etr-optimization';
 export const HOLYDAYS_PRODUCTION = 'https://get-holydays.vercel.app/';
 export const HOLYDAYS_GITHUB = 'https://github.com/joao-mbn/holydays';
@@ -11,13 +9,6 @@ export const TYPES_HOT_PARSER_NPM = 'https://www.npmjs.com/package/@types/hot-fo
 export const HOT_PARSER_NPM = 'https://www.npmjs.com/package/hot-formula-parser';
 export const REACT_EXTRACT_MARKET_PLACE = 'https://marketplace.visualstudio.com/items?itemName=joao-mbn.react-extract';
 export const REACT_EXTRACT_GITHUB = 'https://github.com/joao-mbn/react-extract';
-export const AZURE_CERTIFICATION =
-  'https://learn.microsoft.com/api/credentials/share/en-us/JooNascimento-3680/2B7A12D96DCBCFD2?sharingId';
-export const IELTS_VALIDATION = 'https://ielts.ucles.org.uk/';
-export const EFFECTIVE_ALTRUISM = 'https://www.effectivealtruism.org/';
-export const CAREER_CAPITAL = 'https://80000hours.org/key-ideas-2023/career-capital/';
-export const EARNING_TO_GIVE = 'https://80000hours.org/articles/earning-to-give/';
-export const PRESSING_PROBLEMS = 'https://80000hours.org/problem-profiles/';
 export const BIRTH_DATE = new Date(1997, 1, 17);
 export const CAREER_START_DATE = new Date(2020, 8, 1);
 export const GITHUB_USERNAME = 'joao-mbn';
