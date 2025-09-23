af40f4c4b4bfa8ad56e5aa075a048a4dd59dda31
João Nascimento <joaombn97@gmail.com>
2025-09-23 17:49:26 -0300

    improve header

--- app/components/Header/Links.tsx
+++ app/components/Header/Links.tsx
@@ -1,16 +1,17 @@
 import { LinkedinLink } from '..';
 import { GithubLink } from '../Links/GithubLink';
-import { NavMenu } from './NavMenu';
 
 export function Links() {
   return (
-    <NavMenu className="hidden -translate-y-[0.6rem] sm:flex">
-      <li className="lg:translate-x-[0.5rem]">
-        <GithubLink />
-      </li>
-      <li className="-translate-x-[0.3rem] lg:translate-x-0">
-        <LinkedinLink />
-      </li>
-    </NavMenu>
+    <nav className="hidden -translate-y-[0.6rem] sm:flex">
+      <ul className="flex h-full flex-wrap items-baseline gap-4 lg:gap-6">
+        <li className="lg:translate-x-[0.5rem]">
+          <GithubLink />
+        </li>
+        <li className="-translate-x-[0.3rem] lg:translate-x-0">
+          <LinkedinLink />
+        </li>
+      </ul>
+    </nav>
   );
 }
--- app/components/Header/Navbar.tsx
+++ app/components/Header/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { NavMenu } from './NavMenu';
 
 const routes = [
   { path: '/', value: 'Home' },
@@ -14,14 +13,18 @@ export function Navbar() {
   const activePath = usePathname();
 
   return (
-    <NavMenu>
-      {routes.map(({ path, value }) => (
-        <li key={path}>
-          <Link href={path} className={'p-2 lg:p-3 ' + (path === activePath ? 'text-link-active' : 'text-link')}>
-            {value}
-          </Link>
-        </li>
-      ))}
-    </NavMenu>
+    <nav className="flex-grow sm:flex-grow-0">
+      <ul className="flex h-full flex-wrap items-baseline justify-center gap-4 sm:justify-start lg:gap-6">
+        {routes.map(({ path, value }) => (
+          <li key={path}>
+            <Link
+              href={path}
+              className={'p-2 text-base sm:text-lg lg:p-3 ' + (path === activePath ? 'text-link-active' : 'text-link')}>
+              {value}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
