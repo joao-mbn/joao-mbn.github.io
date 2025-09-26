c1b3dab17b337d3b917447b77f20294e170c128e
João Nascimento <joaombn97@gmail.com>
2025-09-25 22:17:32 -0300

    mail link

--- app/components/Header/Links.tsx
+++ app/components/Header/Links.tsx
@@ -1,11 +1,15 @@
-import { GITHUB, LINKEDIN } from '@/app/utils/constants';
+import { EMAIL, GITHUB, LINKEDIN } from '@/app/utils/constants';
 import { GrLinkedin } from 'react-icons/gr';
+import { RiMailLine } from 'react-icons/ri';
 import { VscGithub } from 'react-icons/vsc';
 import { ExternalLink } from '..';
 
 export function Links() {
   return (
     <nav className="hidden gap-4 sm:flex lg:gap-6">
+      <ExternalLink href={'mailto:' + EMAIL}>
+        <RiMailLine className="h-10 w-10" viewBox="2 1.5 21 21" />
+      </ExternalLink>
       <ExternalLink href={GITHUB}>
         <VscGithub className="h-10 w-10" />
       </ExternalLink>
